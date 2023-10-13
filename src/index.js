7fd10acbc318b4e03a0cd6e49709db62cfae77a3
Abdi Abyan <aa7872@ou.ac.uk>
2023-10-13 19:37:16 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { sendToVercelAnalytics } from './vitals';
 
-ReactDOM.render(
-  <React.StrictMode>
+const root = createRoot(document.getElementById('app'));
+
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </StrictMode>
 );
-
-reportWebVitals(sendToVercelAnalytics);
